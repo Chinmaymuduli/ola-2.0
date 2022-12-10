acd2f77c3a55300f7f8d1e692f994044dc7842dc
Chinmaymuduli <chinmaymuduli0@gmail.com>
2022-12-10 11:08:57 +0530

    google places api key

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,41 @@
 import {Box, ScrollView, Text} from 'native-base';
 import React from 'react';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_API_KEY} from 'utils';
 
 const HomeScreen = () => {
   return (
     <Box flex={1} safeAreaTop bg={'white'}>
-      <ScrollView>
-        <Box>
-          <Text>hello user i am home screen</Text>
-        </Box>
-      </ScrollView>
+      {/* <ScrollView> */}
+      <Box>
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          placeholder="Where are you from ?"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: 'en',
+          }}
+          debounce={400}
+          nearbyPlacesAPI={'GooglePlacesSearch'}
+          enablePoweredByContainer={false}
+          minLength={2}
+        />
+      </Box>
+      <Box>
+        <Text>First Test</Text>
+      </Box>
+      {/* </ScrollView> */}
     </Box>
   );
 };
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyAPIqNSaFurpO6tbgYkOGkO_HiZHN1sxqM';
+// export const GOOGLE_MAPS_API_KEY = 'AIzaSyDf_y80cZLpioZIuSpFSthbS2cr1LzpgEg';
